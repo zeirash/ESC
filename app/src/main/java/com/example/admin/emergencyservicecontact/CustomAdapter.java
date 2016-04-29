13c69c1aa52de5e4642b9118f74b3d93c9cc5cde
Tiffany <tiff.wijaya@yahoo.com>
2016-04-29 22:59:32 +0700

    first commit

---
+++ app/src/main/java/com/example/admin/emergencyservicecontact/CustomAdapter.java
@@ -0,0 +1,44 @@
+package com.example.admin.emergencyservicecontact;
+
+/**
+ * Created by Admin on 27/04/2016.
+ */
+import android.content.ContentValues;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class CustomAdapter extends ArrayAdapter {
+    //why is not working...
+    DbHandler myDbHandler;
+    ArrayList<ListData> listdata;
+
+
+    public CustomAdapter(Context context, String condition) {
+        super(context, R.layout.custom_item);
+        listdata = myDbHandler.getData(condition);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+        View view = inflater.inflate(R.layout.custom_item, parent, false);
+        ListData objectListData = listdata.get(position);
+        //reference tiap widget yg ad di custom_item layout
+        ImageView telephoneIcon = (ImageView) view.findViewById(R.id.iconImage);
+        TextView serviceTypeText = (TextView) view.findViewById(R.id.serviceType_textView);
+        TextView numberText = (TextView) view.findViewById(R.id.number_textView);
+        //set value dri ListData object
+        telephoneIcon.setImageResource(R.drawable.icon);
+        serviceTypeText.setText(objectListData.getServiceType());
+        numberText.setText(objectListData.getPhoneNumber());
+
+        return view;
+    }
+}
